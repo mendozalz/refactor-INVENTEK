567309a05d6cf85128a3c45cf01cedb7d94caeed
menzDev <chetomasrandom@gmail.com>
2024-02-05 00:42:36 -0500

    feat: :zap: mejoras de animaciones y agregando funciones al blog

--- js/cursor.js
+++ js/cursor.js
@@ -1,14 +1,16 @@
 let body = document.querySelector("body");
 let mouse = document.getElementById("effectMouse");
 body.addEventListener("mousemove", (e) => {
-  console.log(e);
   let scale = 1;
+  let backgroundColor = "";
   if (e.target.classList.contains("effect")) {
     scale = 3;
+    backgroundColor = "#02d9f9";
   }
   mouse.style.transform = `
         translateX(${e.clientX}px)
         translateY(${e.clientY}px)
         scale(${scale})
     `;
+  mouse.style.backgroundColor = backgroundColor;
 });
--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@
 
   /* Smooth Scrolling
    * ------------------------------------------------------ */
-  /* var ssSmoothScroll = function () {
+  var ssSmoothScroll = function () {
     $(".smoothscroll").on("click", function (e) {
       var target = this.hash,
         $target = $(target);
@@ -187,42 +187,6 @@
 
           window.location.hash = target;
         });
-    });
-  }; */
-
-  var ssSmoothScroll = function () {
-    $(".smoothscroll").on("click", function (e) {
-      var target = this.hash,
-        $target = $(target);
-
-      e.preventDefault();
-      e.stopPropagation();
-
-      // Verificar si $target es indefinido antes de intentar acceder a su propiedad 'offset'
-      if ($target && $target.offset() && $target.offset().top) {
-        $("html, body")
-          .stop()
-          .animate(
-            {
-              scrollTop: $target.offset().top,
-            },
-            cfg.scrollDuration,
-            "swing"
-          )
-          .promise()
-          .done(function () {
-            // Verificar si el menú está abierto antes de cerrarlo
-            if ($("body").hasClass("menu-is-open")) {
-              $("#header-menu-trigger").trigger("click");
-            }
-
-            window.location.hash = target;
-          });
-      } else {
-        console.error(
-          "El elemento 'target' es indefinido o no tiene la propiedad 'offset'"
-        );
-      }
     });
   };
 
@@ -425,10 +389,3 @@ function activate(e) {
 }
 
 document.addEventListener("click", activate, true);
-
-/* document.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    document.querySelector(".next").click();
-  }, 5000);
-});
- */
